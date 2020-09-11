b474f69c0d6b9b9c99da62e79e3e350105d13aee
vkotsev <vasil@3web.bg>
2020-09-11 16:46:56 +0300

    add a few changes for testing

--- src/constants/toDoItems.js
+++ src/constants/toDoItems.js
@@ -2,11 +2,11 @@ export const toDoItems = [
   {
     _id: 1,
     name:
-      "Да поиграя тенис на маса Да поиграя тенис на маса Да поиграя тенис на маса",
-    isActive: true,
+      "Да поиграя тенис на маса. Да поиграя тенис на маса. Да поиграя тенис на маса",
+    isActive: false,
   },
-  // { _id: 2, name: "Да отида разходя кучето", isActive: true },
-  // { _id: 3, name: "Да си купя палатка", isActive: false },
-  // { _id: 4, name: "Да си заредя телефона", isActive: false },
-  // { _id: 5, name: "Да си довърша книгата", isActive: true },
+  { _id: 2, name: "Да отида разходя кучето", isActive: true, inList: true },
+  { _id: 3, name: "Да си купя палатка", isActive: true, inList: true },
+  { _id: 4, name: "Да си заредя телефона", isActive: false, inList: true },
+  { _id: 5, name: "Да си довърша книгата", isActive: true },
 ];
